28658153edbea5051a3ec8df8594ecf3a9e9ba20
Williams Sissoko <35783824+wcisco17@users.noreply.github.com>
2024-03-21 01:36:50 +0700

    feat(WEEV-25): rm edit details file

--- apps/dashboard/src/lib/create-details/Product/EditProducts/EditProducts.tsx
+++ apps/dashboard/src/lib/create-details/Product/EditProducts/EditProducts.tsx
@@ -1,4 +1,3 @@
-import { ProductImage } from './ProductImage'
 import { BrandVoiceType, ProductType } from '@/database'
 
 type EditProductsProps = {
@@ -10,24 +9,11 @@ type EditProductsProps = {
 }
 
 export const EditProducts = (props: EditProductsProps) => {
-  const { brandVoices, clerkId, organizationId, userId, product } = props
-
   return (
     <div className='min-h-screen p-8'>
       <div className={'mb-4'}>
         <h2 className='text-xl font-semibold'>Edit your your product copywriter</h2>
       </div>
-      {
-        product.image?.map((image) => {
-          return <ProductImage
-            key={image.id}
-            productId={product.id}
-            image={image}
-            organizationId={organizationId}
-            userId={userId}
-          />
-        })
-      }
     </div>
   )
 }
